f4ae7134ce7da2c8eded3d795d7f796c99c836a0
Mikhail Tukai <tukaym@gmail.com>
2016-08-31 15:26:17 +0300

    added benefits block + show more button

---
+++ src/js/partials/show.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.benefits__button').click(function() {
+    $('.benefits__list li:hidden').slice(0, 6).fadeIn();
+    if ($('.benefits__list li').length == $('.benefits__list li:visible').length) {
+      $('.benefits__button').fadeOut();
+    }
+  });
+});
