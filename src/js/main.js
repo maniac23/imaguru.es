45c7b6426555d1fe6981f9f141a09d13df734628
Mikhail Tukai <tukaym@gmail.com>
2016-09-08 16:27:17 +0300

    added slider & filter to events-timetable

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,23 @@
 /*
  * Third party
  */
-//= ../../bower_components/jquery/dist/jquery.js
-//= ../../bower_components/slick-carousel/slick/slick.js
+ //= ../../bower_components/jquery/dist/jquery.js
+ //= ../../bower_components/slick-carousel/slick/slick.js
+
 
 $(document).ready(function() {
   //= partials/menu.js
-  //= partials/info-dropdown.js
-  //= partials/slider.js
-  //= partials/show.js
-  //= partials/image-slider.js
-  //= partials/pricelist-slider.js
-  //= partials/feedback-show.js
-  //= partials/solutions-form.js
-  //= partials/developers-form.js
-  //= partials/accelerator-form.js
-  //= partials/main-text__slider.js
-  //= partials/events-gallery.js
+    //= partials/info-dropdown.js
+    //= partials/slider.js
+    //= partials/show.js
+    //= partials/image-slider.js
+    //= partials/pricelist-slider.js
+    //= partials/feedback-show.js
+    //= partials/solutions-form.js
+    //= partials/developers-form.js
+    //= partials/accelerator-form.js
+    //= partials/main-text__slider.js
+    //= partials/timetable-slider.js
+    //= partials/events-gallery.js
+    //= partials/filter.js
 });
