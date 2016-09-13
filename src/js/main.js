e27c94b84a64818f2c91208dfbe13651256a8b47
Mikhail Tukai <tukaym@gmail.com>
2016-09-13 16:51:44 +0300

    fixed menu

--- src/js/main.js
+++ src/js/main.js
@@ -6,19 +6,20 @@
 
 
 $(document).ready(function() {
-    //= partials/info-dropdown.js
-    //= partials/slider.js
-    //= partials/show.js
-    //= partials/image-slider.js
-    //= partials/pricelist-slider.js
-    //= partials/feedback-show.js
-    //= partials/solutions-form.js
-    //= partials/developers-form.js
-    //= partials/accelerator-form.js
-    //= partials/main-text__slider.js
-    //= partials/timetable-slider.js
-    //= partials/events-gallery.js
-    //= partials/filter.js
-    //= partials/course-registration-form.js
-    //= partials/equal-height.js
+  //= partials/menu.js
+  //= partials/info-dropdown.js
+  //= partials/slider.js
+  //= partials/show.js
+  //= partials/image-slider.js
+  //= partials/pricelist-slider.js
+  //= partials/feedback-show.js
+  //= partials/solutions-form.js
+  //= partials/developers-form.js
+  //= partials/accelerator-form.js
+  //= partials/main-text__slider.js
+  //= partials/timetable-slider.js
+  //= partials/events-gallery.js
+  //= partials/filter.js
+  //= partials/course-registration-form.js
+  //= partials/equal-height.js
 });
